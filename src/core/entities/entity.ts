f73973994f88736504422535003608aa77f78ee0
Cleyson Lima <cleysonph@gmail.com>
2024-08-23 16:08:48 -0300

    id das entidades

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,11 +1,12 @@
 import { randomUUID } from "node:crypto"
+import { UniqueEntityID } from "../../domain/entities/value-objects/unique-entity-id"
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityID
   protected props: Props
 
-  constructor(props: Props, id?: string) {
-    this._id = id ?? randomUUID()
+  constructor(props: Props, id?: UniqueEntityID) {
+    this._id = new UniqueEntityID()
     this.props = props
   }
 
